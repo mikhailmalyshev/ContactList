b3b941285062043415fe49bb636a01c543f2a3a6
Mikhail Malyshev <malyshev090994@gmail.com>
2020-03-17 11:36:07 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Михаил Малышев on 17/03/2020.
+//  Copyright © 2020 Mikhail Malyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
